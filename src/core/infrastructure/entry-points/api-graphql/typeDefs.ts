c2e3962fbe17b822aa5ecf7ab96f817dc9fcd377
Victor Rosales <rosalesvictor.dev@gmail.com>
2024-02-18 12:14:42 -0500

    style: add Sixtax Highlithging for grapql typeDefs

--- src/core/infrastructure/entry-points/api-graphql/typeDefs.ts
+++ src/core/infrastructure/entry-points/api-graphql/typeDefs.ts
@@ -1,4 +1,8 @@
-const typeDefs = `
+import { Implementation } from '../../implementations'
+
+const gql = Implementation.Util.raw
+
+const typeDefs = gql`
 # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 # This "Book" type defines the queryable fields for every book in our data source.
 
